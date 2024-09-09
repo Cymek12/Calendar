0e457fbf7d4802f12fd8df72d698ad2f2a623f07
Kuba C <jak.cymerman@gmail.com>
2024-09-09 17:40:58 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Scheduler from 'devextreme-react/scheduler';
+import { locale, loadMessages } from 'devextreme/localization';
+import plMessages from 'devextreme/localization/messages/pl.json';
+import { db } from './firebaseConfig';
+import { collection, getDocs, addDoc, updateDoc, deleteDoc, doc } from 'firebase/firestore';
 import './App.css';
 
-function App() {
+loadMessages(plMessages);
+locale('pl');
+
+const App = () => {
+  const [appointments, setAppointments] = useState([]);
+
+  const fetchAppointments = async () => {
+    const querySnapshot = await getDocs(collection(db, 'appointments'));
+    const appointmentsData = querySnapshot.docs.map(doc => ({
+      ...doc.data(),
+      id: doc.id, 
+      startDate: doc.data().startDate.toDate(),
+      endDate: doc.data().endDate.toDate(),
+      description: doc.data().description || '',
+      recurrenceRule: doc.data().recurrenceRule || ''
+    }));
+    setAppointments(appointmentsData);
+  };
+
+  const onAppointmentAdded = async (e) => {
+    const newAppointment = {
+      text: e.appointmentData.text || '',
+      startDate: e.appointmentData.startDate,
+      endDate: e.appointmentData.endDate,
+      description: e.appointmentData.description || '',
+      recurrenceRule: e.appointmentData.recurrenceRule || ''
+    };
+    await addDoc(collection(db, 'appointments'), newAppointment);
+    fetchAppointments();
+  };
+  
+
+  const onAppointmentUpdated = async (e) => {
+    const appointmentDoc = doc(db, 'appointments', e.appointmentData.id);
+    await updateDoc(appointmentDoc, {
+      text: e.appointmentData.text || '',
+      startDate: e.appointmentData.startDate,
+      endDate: e.appointmentData.endDate,
+      description: e.appointmentData.description || '',
+      recurrenceRule: e.appointmentData.recurrenceRule || ''
+    });
+    fetchAppointments();
+  };
+
+  const onAppointmentDeleted = async (e) => {
+    const appointmentDoc = doc(db, 'appointments', e.appointmentData.id);
+    await deleteDoc(appointmentDoc);
+    fetchAppointments();
+  };
+
+  useEffect(() => {
+    fetchAppointments();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1><b>Kalendarz</b></h1>
+      <Scheduler
+        dataSource={appointments}
+        defaultCurrentDate={new Date()}
+        adaptivityEnabled={true}
+        views={[
+          { type: 'day', name: 'Dzień' },
+          { type: 'week', name: 'Tydzień' },
+          { type: 'month', name: 'Miesiąc' }
+        ]}
+        defaultCurrentView="week"
+        editing={{
+          allowAdding: true,
+          allowDeleting: true,
+          allowUpdating: true
+        }}
+        onAppointmentAdded={onAppointmentAdded}
+        onAppointmentUpdated={onAppointmentUpdated}
+        onAppointmentDeleted={onAppointmentDeleted}
+        timeZone="Europe/Warsaw"
+        height={600}
+      />
     </div>
   );
 }
